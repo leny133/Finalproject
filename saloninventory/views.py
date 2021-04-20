2f5dec193ca907f38288ab49bf40c159160b9ca2
leny133 <63651131+leny133@users.noreply.github.com>
2021-04-20 15:26:41 -0400

    login and register templates

--- saloninventory/views.py
+++ saloninventory/views.py
@@ -1,7 +1,75 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import User
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+
+    # Authenticated users view their inbox
+    if request.user.is_authenticated:
+        return HttpResponse("You are loggeed in")
+
+    # Everyone else is prompted to sign in
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "saloninventory/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            
+        except IntegrityError:
+            return render(request, "saloninventory/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "saloninventory/register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "saloninventory/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "saloninventory/login.html")
