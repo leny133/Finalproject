7047698b878dcfb6a9f385df262117eea6168564
leny133 <63651131+leny133@users.noreply.github.com>
2021-05-05 15:54:16 -0400

    models

--- saloninventory/models.py
+++ saloninventory/models.py
@@ -12,9 +12,16 @@ class Products(models.Model):
     units = models.IntegerField(null=True)
     amountperunit = models.DecimalField(max_digits=10, decimal_places=2)
     unitprice = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, relatedname="pdescription")
+    image = models.URLField(null=True)
 
 class Services(models.Model):
     servowner = models.ForeignKey("User", on_delete=models.CASCADE, related_name="servowner")
-    client = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     service = models.CharField(max_length=50)
+    description = models.CharField(max_length=250, relatedname="sdescription")
+
+class Sale(models.Model):
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    sold_products = models.ManyToManyField("Products", on_delete=models.CASCADE, related_name="sold_products")
+    sold_services = models.ManyToManyField("Services", on_delete=models.CASCADE, related_name="sold_services")
