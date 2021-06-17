e56cb2ad05623e14e1ff3b8f8b09c245b52270cf
leny133 <63651131+leny133@users.noreply.github.com>
2021-06-17 13:22:06 -0400

    something done

--- saloninventory/static/saloninventory/newsale.js
+++ saloninventory/static/saloninventory/newsale.js
@@ -1,4 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector("#showproduct")
+    .addEventListener("click", () => showproduct());
+  document
+    .querySelector("#showservice")
+    .addEventListener("click", () => showservice());
+  load_products_services();
 });
+var allproducts;
+var allservices;
+function load_products_services() {
+  fetch("/myproducts")
+    .then((response) => response.json())
+    .then((myproducts) => {
+      allproducts = myproducts;
+      for (product in myproducts) {
+        var y = document.createElement("option");
+        y.setAttribute("value", `${myproducts[product].Product_name}`);
+        y.innerHTML = `${myproducts[product].Product_name}(${myproducts[product].Measure_type}) `;
+        document.querySelector("#products").append(y);
+      }
+    });
+    fetch("/myservices")
+      .then((response) => response.json())
+      .then((myservices) => {
+        allservices = myservices;
+        console.log(myservices)
+        for (service in myservices) {
+          var y = document.createElement("option");
+          y.setAttribute("value", `${myservices[service].Service_name}`);
+          y.innerHTML = `${myservices[service].Service_name}`;
+          document.querySelector("#services").append(y);
+        }
+      });
+}
+
+function showproduct() {
+  document.querySelector("#cartproduct-view").style.display = "block";
+  document.querySelector("#cartservice-view").style.display = "none";
+  console.log(allproducts);
+}
+function showservice() {
+  document.querySelector("#cartproduct-view").style.display = "none";
+  document.querySelector("#cartservice-view").style.display = "block";
+}
