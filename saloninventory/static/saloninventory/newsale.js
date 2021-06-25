9269d4281443e3f3a8df1e193448d366acaee097
leny133 <63651131+leny133@users.noreply.github.com>
2021-06-25 15:44:26 -0400

    creating cart

--- saloninventory/static/saloninventory/newsale.js
+++ saloninventory/static/saloninventory/newsale.js
@@ -1,3 +1,5 @@
+let cartP = [];
+let cartS = [];
 document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector("#showproduct")
@@ -5,41 +7,67 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector("#showservice")
     .addEventListener("click", () => showservice());
-  load_products_services();
+  document
+    .querySelector("#productCart")
+    .addEventListener("click", () => addToCartP());
+  document
+    .querySelector("#serviceCart")
+    .addEventListener("click", () => addToCartS());
+  
+  fetch_products().then((Products) => {
+    load_products(Products);
+  });
+  fetch_services().then((services) => {
+    load_services(services);
+  });
 });
-var allproducts;
-var allservices;
-function load_products_services() {
-  fetch("/myproducts")
-    .then((response) => response.json())
-    .then((myproducts) => {
-      allproducts = myproducts;
-      for (product in myproducts) {
-        var y = document.createElement("option");
-        y.setAttribute("value", `${myproducts[product].Product_name}`);
-        y.innerHTML = `${myproducts[product].Product_name}(${myproducts[product].Measure_type}) `;
-        document.querySelector("#products").append(y);
-      }
-    });
-    fetch("/myservices")
-      .then((response) => response.json())
-      .then((myservices) => {
-        allservices = myservices;
-        for (service in myservices) {
-          var y = document.createElement("option");
-          y.setAttribute("value", `${myservices[service].Service_name}`);
-          y.innerHTML =`${myservices[service].Service_name}`;
-          document.querySelector("#services").append(y);
-        }
-      });
+
+async function fetch_products() {
+  const response = await fetch("/myproducts");
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  Products = await response.json();
+  return Products;
+}
+async function fetch_services() {
+  const response = await fetch("/myservices");
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  services = await response.json();
+  return services;
+}
+
+function load_products(Products) {
+  for (product in Products) {
+    var y = document.createElement("option");
+    y.setAttribute("value", `${Products[product].id}`);
+    y.innerHTML = `${Products[product].Product_name}(${Products[product].Measure_type}) `;
+    document.querySelector("#products").append(y);
+  }
+}
+function load_services(services) {
+  for (service in services) {
+    var y = document.createElement("option");
+    y.setAttribute("value", `${services[service].id}`);
+    y.innerHTML = `${services[service].Service_name}`;
+    document.querySelector("#services").append(y);
+  }
 }
 
 function showproduct() {
   document.querySelector("#cartproduct-view").style.display = "block";
   document.querySelector("#cartservice-view").style.display = "none";
-  
 }
 function showservice() {
   document.querySelector("#cartproduct-view").style.display = "none";
   document.querySelector("#cartservice-view").style.display = "block";
 }
+function addToCartP() {
+  const prodAdd =document.querySelector("#products").value 
+  
+}
+function addToCartS() {}
