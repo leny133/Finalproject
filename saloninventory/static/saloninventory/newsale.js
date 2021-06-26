79d7feb68affb49ec04a7b99fcd3381ec1c2e9e4
leny133 <63651131+leny133@users.noreply.github.com>
2021-06-26 14:21:30 -0400

    submit not working

--- saloninventory/static/saloninventory/newsale.js
+++ saloninventory/static/saloninventory/newsale.js
@@ -7,9 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector("#showservice")
     .addEventListener("click", () => showservice());
-  document
-    .querySelector("#productCart")
-    .addEventListener("click", () => addToCartP());
+  
   document
     .querySelector("#serviceCart")
     .addEventListener("click", () => addToCartS());
@@ -29,6 +27,7 @@ async function fetch_products() {
     throw new Error(message);
   }
   Products = await response.json();
+  console.log(Products)
   return Products;
 }
 async function fetch_services() {
@@ -38,6 +37,7 @@ async function fetch_services() {
     throw new Error(message);
   }
   services = await response.json();
+  console.log(services)
   return services;
 }
 
@@ -45,6 +45,9 @@ function load_products(Products) {
   for (product in Products) {
     var y = document.createElement("option");
     y.setAttribute("value", `${Products[product].id}`);
+    y.setAttribute("id",`p${Products[product].id}`)
+    y.setAttribute("name",`${Products[product].Product_name}`)
+    y.setAttribute("price", `${Products[product].Unit_price/Products[product].Amount_per_unit}`);
     y.innerHTML = `${Products[product].Product_name}(${Products[product].Measure_type}) `;
     document.querySelector("#products").append(y);
   }
@@ -52,7 +55,8 @@ function load_products(Products) {
 function load_services(services) {
   for (service in services) {
     var y = document.createElement("option");
-    y.setAttribute("value", `${services[service].id}`);
+    y.setAttribute("value", `${services[service].Service_id}`);
+    y.setAttribute("priceS", `${services[service].Service_price}`);
     y.innerHTML = `${services[service].Service_name}`;
     document.querySelector("#services").append(y);
   }
@@ -68,9 +72,25 @@ function showservice() {
 }
 function addToCartP() {
   
-  fetch_products().then((Products) => {
-    const prodAdd =document.querySelector("#products").value 
+  
+    const prodId =document.querySelector("#products").value 
+    const prodPrice =document.getElementById(`p${prodId}`).getAttribute('price')
+    const prodAmount = document.getElementById("productAmount").value
+    const prodName = document.getElementById(`p${prodId}`).getAttribute('name')
+    const tPSale = prodPrice*prodAmount;
+    var y = document.createElement("table")
+    y.innerHTML = `
+    <tr>
+            <td>${prodName} </td>
+            <td> ${prodPrice} </td>
+            <td> ${prodAmount}</td>
+            <td> ${tPSale}</td>
+    </tr>
+          `   
+    document.getElementById("cartList").append(y)
+    console.log(prodId)
+    console.log(tPSale, prodAmount) 
     
-  });
+  
 }
 function addToCartS() {}
