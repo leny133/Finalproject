04319d378b7be52d0eecfdb8b85d2466665369e6
leny133 <63651131+leny133@users.noreply.github.com>
2021-06-30 15:52:01 -0400

    edit price button

--- saloninventory/static/saloninventory/newsale.js
+++ saloninventory/static/saloninventory/newsale.js
@@ -14,26 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector("#serviceCart")
     .addEventListener("click", () => addToCartS());
-  document
-    .querySelector("#emptycart")
-    .addEventListener("click", () => {sessionStorage.clear(); location.href ="/"; });
+  document.querySelector("#emptycart").addEventListener("click", () => {
+    sessionStorage.clear();
+    location.href = "/";
+  });
   document.querySelector("#Cart").style.display = "none";
   document.querySelector("#emptycart").style.display = "none";
   cartP = JSON.parse(cartP);
   console.log(document.querySelector("#userid").name);
-  if(cartP!=null){
+  if (cartP != null) {
     loadCart();
     document.querySelector("#Cart").style.display = "block";
     document.querySelector("#emptycart").style.display = "block";
   }
-  
+
   fetch_products().then((Products) => {
     load_products(Products);
   });
   fetch_services().then((services) => {
     load_services(services);
   });
-
 });
 
 async function fetch_products() {
@@ -53,7 +53,7 @@ async function fetch_services() {
     throw new Error(message);
   }
   services = await response.json();
-  console.log(services)
+  console.log(services);
   return services;
 }
 
@@ -61,9 +61,14 @@ function load_products(Products) {
   for (product in Products) {
     var y = document.createElement("option");
     y.setAttribute("value", `${Products[product].id}`);
-    y.setAttribute("id",`p${Products[product].id}`)
-    y.setAttribute("name",`${Products[product].Product_name}`)
-    y.setAttribute("price", `${(Products[product].Unit_price/Products[product].Amount_per_unit).toFixed(2)}`);
+    y.setAttribute("id", `p${Products[product].id}`);
+    y.setAttribute("name", `${Products[product].Product_name}`);
+    y.setAttribute(
+      "price",
+      `${(
+        Products[product].Unit_price / Products[product].Amount_per_unit
+      ).toFixed(2)}`
+    );
     y.innerHTML = `${Products[product].Product_name}(${Products[product].Measure_type}) `;
     document.querySelector("#products").append(y);
   }
@@ -72,7 +77,9 @@ function load_services(services) {
   for (service in services) {
     var y = document.createElement("option");
     y.setAttribute("value", `${services[service].Service_id}`);
-    y.setAttribute("priceS", `${services[service].Service_price}`);
+    y.setAttribute("id", `s${services[service].Service_id}`);
+    y.setAttribute("name", `${services[service].Service_name}`);
+    y.setAttribute("price", `${services[service].Service_price}`);
     y.innerHTML = `${services[service].Service_name}`;
     document.querySelector("#services").append(y);
   }
@@ -87,55 +94,104 @@ function showservice() {
   document.querySelector("#cartservice-view").style.display = "block";
 }
 function addToCartP() {
-  
-  
-    const prodId =document.querySelector("#products").value 
-    const prodPrice =document.getElementById(`p${prodId}`).getAttribute('price')
-    const prodAmount = document.getElementById("productAmount").value
-    const prodName = document.getElementById(`p${prodId}`).getAttribute('name')
-    const tPSale = (prodPrice*prodAmount).toFixed(2);
-    var itemcount = sessionStorage.getItem(`${usr}itemcount`);
-    var cartproducts = {
-      pid:prodId,
-      Product_Name:prodName,
-      Price:prodPrice,
-      Amount:prodAmount
-    };
-    
-    if (cartP == null){
-      cartP = [];
-      itemcount=0;
-      
-    }else{
+  const prodId = document.querySelector("#products").value;
+  const prodPrice = document.getElementById(`p${prodId}`).getAttribute("price");
+  const prodAmount = document.getElementById("productAmount").value;
+  const prodName = document.getElementById(`p${prodId}`).getAttribute("name");
+  const tPSale = (prodPrice * prodAmount).toFixed(2);
+  var itemcount = sessionStorage.getItem(`${usr}itemcount`);
+  var cartproducts = {
+    pid: prodId,
+    Product_Name: prodName,
+    Price: prodPrice,
+    Amount: prodAmount,
+    Sale: tPSale,
+  };
 
-      itemcount=parseInt(itemcount)+1;
-      
-    }
-    
-    console.log(itemcount)
-    console.log(cartproducts)
-    cartP[itemcount]=cartproducts;
-    console.log(cartP);
-    var jsonStr = JSON.stringify(cartP);
-    
-    sessionStorage.setItem(`${usr}itemcount`,itemcount)
-    sessionStorage.setItem(`${usr}cartproducts`,jsonStr)
-    location.href = "/newSale";
+  if (cartP == null) {
+    cartP = [];
+    itemcount = 0;
+  } else {
+    itemcount = parseInt(itemcount) + 1;
+  }
+
+  console.log(itemcount);
+  console.log(cartproducts);
+  cartP[itemcount] = cartproducts;
+  console.log(cartP);
+  var jsonStr = JSON.stringify(cartP);
+
+  sessionStorage.setItem(`${usr}itemcount`, itemcount);
+  sessionStorage.setItem(`${usr}cartproducts`, jsonStr);
+  location.href = "/newSale";
 }
-function addToCartS() {};
-function loadCart(){
-  
-  for(i=0;i<cartP.length;i++){
-   var y = document.createElement("tr"); 
-   y.innerHTML = `
+function addToCartS() {
+  const prodId = document.querySelector("#services").value;
+  const prodPrice = document.getElementById(`s${prodId}`).getAttribute("price");
+  const prodAmount = 1;
+  const prodName = document.getElementById(`s${prodId}`).getAttribute("name");
+  const tPSale = (prodPrice * prodAmount).toFixed(2);
+  var itemcount = sessionStorage.getItem(`${usr}itemcount`);
+  var cartproducts = {
+    pid: prodId,
+    Product_Name: prodName,
+    Price: prodPrice,
+    Amount: prodAmount,
+    Sale: tPSale,
+  };
+
+  if (cartP == null) {
+    cartP = [];
+    itemcount = 0;
+  } else {
+    itemcount = parseInt(itemcount) + 1;
+  }
+
+  console.log(itemcount);
+  console.log(cartproducts);
+  cartP[itemcount] = cartproducts;
+  console.log(cartP);
+  var jsonStr = JSON.stringify(cartP);
+
+  sessionStorage.setItem(`${usr}itemcount`, itemcount);
+  sessionStorage.setItem(`${usr}cartproducts`, jsonStr);
+  location.href = "/newSale";
+}
+function loadCart() {
+  var total_sale = 0;
+  for (i = 0; i < cartP.length; i++) {
+    var y = document.createElement("tr");
+    y.innerHTML = `
     
             <td>${cartP[i].Product_Name} </td>
             <td> $${cartP[i].Price} </td>
             <td> ${cartP[i].Amount}</td>
-            <td> $${cartP[i].Price*cartP[i].Amount}</td>
+            <td> $${(cartP[i].Price * cartP[i].Amount).toFixed(2)}</td>
+            <td> <button id="ed${cartP[i].pid}" name="${cartP[i].pid}"
+             onclick="edit(${cartP[i].pid},)">
+            Edit
+            </button></td>
+          `;
+    document.getElementById("Cart").append(y);
+    total_sale = parseFloat(cartP[i].Sale) + total_sale;
+  }
+  y = document.createElement("tr");
+  y.innerHTML = `
+    
+            <td> </td>
+            <td> </td>
+            <td> Total: </td>
+            <td> $${total_sale}</td>
     
           `;
-  document.getElementById("Cart").append(y); 
-  }
+  document.getElementById("Cart").append(y);
+}
+function edit(prod_id){
+  var x = parseFloat(prompt("Enter price change", "0"));
+  const isInteger = /^[0-9.,]+$/;
+if(isInteger.test(x)) {
   
-};
+}else{
+  edit(prod_id)
+}
+}
